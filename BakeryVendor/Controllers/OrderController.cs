951eba80d50da1751e20ed7b975022eedad3ba09
James Fox <jimmyfox00@gmail.com>
2022-05-13 11:18:20 -0700

    Added basic routing for Order controller

--- BakeryVendor/Controllers/OrderController.cs
+++ BakeryVendor/Controllers/OrderController.cs
@@ -1,9 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
+using BakeryVendor.Models;
+using System;
+using System.Collections.Generic;
 
 namespace BakeryVendor.Controllers
 {
     public class OrderController : Controller
     {
+      [HttpGet("/orders")]
+      public ActionResult Index()
+      {
+        List<Order> orders = Order.GetAll();
+        return View(orders);
+      }
 
+      [HttpGet("/orders/new")]
+      public ActionResult New()
+      {
+        List<Vendor> vendors = Vendor.GetAll();
+        return View(vendors);
+      }
+
+      [HttpPost("/orders")]
+      public ActionResult Create(int orderVendor, string orderTitle, string orderCost)
+      {
+        Vendor vendor = Vendor.Find(orderVendor);
+        Order order = new Order(vendor, orderTitle, int.Parse(orderCost));
+        return Redirect($"/vendors/{vendor.Id}");
+      }
+
+      [HttpGet("/orders/{id}")]
+      public ActionResult Show(int id)
+      {
+        Order order = Order.Find(id);
+        return View(order);
+      }
     }
 }
