9134b7f162865a1a69feaf0b1f763b1ebb214130
James Fox <jimmyfox00@gmail.com>
2022-05-13 09:11:25 -0700

    Add Order model

--- BakeryVendor/Models/Order.cs
+++ BakeryVendor/Models/Order.cs
@@ -1,7 +1,39 @@
+using System;
+using System.Collections.Generic;
+
 namespace BakeryVendor.Models
 {
   public class Order
   {
-    
+    private static List<Order> _orders = new List<Order>{};
+    public Vendor Vendor { get; set; }
+    public int Id { get; }
+    public string Title { get; set; }
+    public int Cost { get; set; }
+    public DateTime DateCreated { get; set; }
+
+    public Order(Vendor vendor, string title, int cost)
+    {
+      _orders.Add(this);
+      Vendor = vendor;
+      Id = _orders.Count;
+      Title = title;
+      Cost = cost;
+      DateCreated = DateTime.Now;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _orders;
+    }
+
+    public static void ClearAll()
+    {
+      _orders.Clear();
+    }
+    public static Order Find(int searchId)
+    {
+      return _orders[searchId-1];
+    }
   }
 }
--- BakeryVendor/Models/Vendor.cs
+++ BakeryVendor/Models/Vendor.cs
@@ -1,7 +1,41 @@
+using System.Collections.Generic;
 namespace BakeryVendor.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _vendors = new List<Vendor>{};
+    public int Id { get;}
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Order> Orders { get; set; }
 
+    public Vendor(string name, string description)
+    {
+      _vendors.Add(this);
+      Id = _vendors.Count;
+      Name = name;
+      Description = description;
+      Orders = new List<Order>{};
+    }
+
+    public static void ClearAll()
+    {
+      _vendors.Clear();
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _vendors;
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _vendors[searchId-1];
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
   }
 }
